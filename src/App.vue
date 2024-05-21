8b08d63c366eefa6f42fc7075be71e39cb209dc0
FodorIustin <126153569+FodorIustin@users.noreply.github.com>
2024-05-21 08:40:27 +0300

    cards

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Card v-for="product in products" :key="product.ID" :product="product" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Card from './components/Card.vue'; 
+
+const products = ref([
+  {
+    ID: 1,
+    Prenume: "Maria",
+    Nume: "Anders",
+    City: "Berlin",
+    "Postal Code": 12209,
+    Country: "Germany",
+  },
+  {
+    ID: 2,
+    Prenume: "Ana",
+    Nume: "Trujillo",
+    City: "México D.F.",
+    "Postal Code": 15021,
+    Country: "Mexico",
+  },
+]);
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <div class="details">
+      <h2 class="text-xl font-bold mb-2">ID: {{ product.ID }}</h2>
+      <p>Prenume: {{ product.Prenume }}</p>
+      <p>Nume: {{ product.Nume }}</p>
+      <p>City: {{ product.City }}</p>
+      <p>Postal Code: {{ product["Postal Code"] }}</p>
+      <p>Country: {{ product.Country }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { product } = defineProps(['product']);
+</script>
+
+<style scoped>
+.card {
+  @apply border p-4 m-4;
+}
+
+.details {
+  @apply text-center;
+}
+</style>
